9f273af2e1f3767bccd3fd1dfbf8565d3e8bb3e2
Anthony Mogotlane <almogotlane@gmail.com>
2022-05-24 05:41:03 +0200

    First commit
    create folder and file

---
+++ test/matchingDays.test.js
@@ -0,0 +1,7 @@
+describe("The matchingDays function", () => {
+    describe("set first date values", () => {
+        it("should return the input date", () => {
+            assert.equal(1, 0);
+        })
+    })
+})
