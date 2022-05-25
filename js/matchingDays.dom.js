1bcf8d59c708844fd88323cfc0a61c80bd708836
Anthony <almogotlane@gmail.com>
2022-05-25 14:45:53 +0200

    modify matchingDays.dom.js
    modify handlebars custom helper and add more functionality to the logic

--- js/matchingDays.dom.js
+++ js/matchingDays.dom.js
@@ -3,39 +3,44 @@ const firstDate = document.querySelector("#first-date");
 const secondDate = document.querySelector("#second-date");
 
 //instance of matchingDays function
-let matchingDay = matchingDays();
+const matchingDay = matchingDays();
 
 firstDate.addEventListener("change", () => {
     matchingDay.setFirstDate(firstDate.value);
+    matchingDay.setSecondDate(secondDate.value);
+    console.log(matchingDay.getDayColors())
+    weekDaysTemplate(matchingDay.getFirstDay(), matchingDay.getSecondDay(), matchingDay.getDayColors());
 })
 
 secondDate.addEventListener("change", () => {
+    matchingDay.setFirstDate(firstDate.value);
     matchingDay.setSecondDate(secondDate.value);
+
+    weekDaysTemplate(matchingDay.getFirstDay(), matchingDay.getSecondDay(), matchingDay.getDayColors());
 })
 
-
-function weekDaysTemplate() {
+function weekDaysTemplate(theDayOne, theDayTwo, theColors) {
     // compile the template
     let daysTemplate = document.querySelector(".days-content").innerHTML;
+    let theColor = theColors;
     // custom helper for color behavior
     Handlebars.registerHelper('color', str =>  { 
-        return `<li class="red">${str}</li>`
+        if(str === theDayOne) {
+            return `<li class="${theColor[0]} ${theColor}">${str}</li>`;
+        } else if (str === theDayTwo) {
+            return `<li class=${theColor[1]} ${theColor}>${str}</li>`;
+        } else {
+            return `<li>${str}</li>`;
+        }
     })
     //template function
     let daysTemplateFunction = Handlebars.compile(daysTemplate);
 
     let weekDays = {
-        monday : "Monday",
-        tuesday: "Tuesday",
-        wednesday: "Wednesday",
-        thursday: "Thursday",
-        friday: "Friday",
-        saturday: "Saturday",
-        sunday: "Sunday"
+        listOfDays: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     }
 
     document.querySelector(".days-container").innerHTML = daysTemplateFunction(weekDays);
 }
 weekDaysTemplate();
-
 
