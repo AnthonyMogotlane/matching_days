5c43254fc64f18de3a8242a6d86cf8e91ac98d6d
Anthony Mogotlane <almogotlane@gmail.com>
2022-05-26 00:05:52 +0200

    modify matchingDays.dom.js
    add data to localStorage and initiate the state with localStorage values

--- js/matchingDays.dom.js
+++ js/matchingDays.dom.js
@@ -5,18 +5,35 @@ const secondDate = document.querySelector("#second-date");
 //instance of matchingDays function
 const matchingDay = matchingDays();
 
+//values from localStorage
+let theFirstDate = (localStorage.getItem("firstDate") !== null) ? JSON.parse(localStorage.getItem("firstDate")) : "";
+let theSecondDate = (localStorage.getItem("secondDate") !== null) ? JSON.parse(localStorage.getItem("secondDate")) : "";
+let theColors = (localStorage.getItem("colors") !== null) ? JSON.parse(localStorage.getItem("colors")) : "";
+
+firstDate.value = theFirstDate;
+secondDate.value = theSecondDate;
+matchingDay.setFirstDate(firstDate.value);
+matchingDay.setSecondDate(secondDate.value);
+
+//get the first date event
 firstDate.addEventListener("change", () => {
     matchingDay.setFirstDate(firstDate.value);
     matchingDay.setSecondDate(secondDate.value);
 
     weekDaysTemplate(matchingDay.getFirstDay(), matchingDay.getSecondDay(), matchingDay.getDayColors());
-})
 
+    localStorage.setItem("firstDate", JSON.stringify(matchingDay.getFirstDate()))
+    localStorage.setItem("colors", JSON.stringify(matchingDay.getDayColors()))
+})
+//get the second date event
 secondDate.addEventListener("change", () => {
     matchingDay.setFirstDate(firstDate.value);
     matchingDay.setSecondDate(secondDate.value);
 
     weekDaysTemplate(matchingDay.getFirstDay(), matchingDay.getSecondDay(), matchingDay.getDayColors());
+
+    localStorage.setItem("secondDate", JSON.stringify(matchingDay.getSecondDate()))
+    localStorage.setItem("colors", JSON.stringify(matchingDay.getDayColors()))
 })
 
 function weekDaysTemplate(theDayOne, theDayTwo, theColors) {
@@ -38,10 +55,11 @@ function weekDaysTemplate(theDayOne, theDayTwo, theColors) {
     let daysTemplateFunction = Handlebars.compile(daysTemplate);
 
     let weekDays = {
-        listOfDays: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        listOfDays: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     }
 
     document.querySelector(".days-container").innerHTML = daysTemplateFunction(weekDays);
 }
-weekDaysTemplate();
+weekDaysTemplate(matchingDay.getFirstDay(), matchingDay.getSecondDay(), matchingDay.getDayColors());
+
 
